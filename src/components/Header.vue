c4abd234c933f042d0f06641dfb7514683926765
Dmitriy <mysl1m9543@gmail.com>
2022-04-19 16:22:03 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Система электронной очереди в поликлинику</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
